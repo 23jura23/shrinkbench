0d1da56fa5fd0ba4023f42ab9987e706a706a42b
ilyaonoff <ilyaonof@yandex.ru>
2022-06-03 19:58:31 +0300

    Make iterative pruning possible

--- pruning/modules.py
+++ pruning/modules.py
@@ -43,6 +43,9 @@ class MaskedModule(nn.Module):
         self.weight = layer.weight
         self.bias = layer.bias
 
+        self.weight_mask = torch.ones_like(self.weight)
+        self.bias_mask = torch.ones_like(self.bias)
+
         # use register_buffer so model.to(device) works on fixed tensors like masks
         self.register_buffer("weight_mask", None)
         self.register_buffer("bias_mask", None)
@@ -64,15 +67,15 @@ class MaskedModule(nn.Module):
 
         # Multiply weights by masks so metrics can count nonzeros
         weight_mask = _ensure_tensor(weight_mask)
-        self.weight_mask = _same_device(weight_mask, self.weight)
-        self.weight.data.mul_(weight_mask)
+        self.weight_mask *= _same_device(weight_mask, self.weight)
+        self.weight.data.mul_(self.weight_mask)
 
         if bias_mask is not None:
             bias_mask = _ensure_tensor(bias_mask)
             assert self.bias is not None, "Provided layer must have bias for it to be masked"
             assert _same_shape(bias_mask, self.bias), f"Bias Mask must match dimensions"
-            self.bias_mask = _same_device(bias_mask, self.bias)
-            self.bias.data.mul_(bias_mask)
+            self.bias_mask *= _same_device(bias_mask, self.bias)
+            self.bias.data.mul_(self.bias_mask)
 
 
 class LinearMasked(MaskedModule):
