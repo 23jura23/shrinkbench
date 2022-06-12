830b96a735728f1ce930f4eb4f3ee1e686733624
Худяков Юрий <catchitoo@mail.ru>
2022-06-12 16:44:25 +0300

    Add cifar 100 pretrained (2 attempt)

--- models/pretrained.py
+++ models/pretrained.py
@@ -2,10 +2,6 @@ import torch
 
 
 def pretrained_weights(model):
-    if model == "cifar100_resnet20":
-        url = "https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet20-23dac2f1.pt"
-    else:
-        url = f'https://raw.githubusercontent.com/JJGO/shrinkbench-models/master/cifar10/{model}.th'
-
+    url = f'https://raw.githubusercontent.com/JJGO/shrinkbench-models/master/cifar10/{model}.th'
     print(url)
     return torch.hub.load_state_dict_from_url(url, map_location='cpu')
